dda2f85bf8eb6e2c6d2f53dff22729180e72afe0
Judy <judy.zhu@bindo.com>
2018-03-27 01:01:06 +0800

    process

--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,10 +1,31 @@
 import React from 'react'
-// import { NavBar } from 'antd-mobile'
+import { NavBar, InputItem, TextAreaItem } from 'antd-mobile'
+import { AvatarSelector } from '../../component/avatar-selector/avatar-selector'
 
 class BossInfo extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      title: '',
+    }
+  }
+
+  onChange(key,val){
+    this.setState({[key]:val})
+  }
 
   render(){
-    return <h2>fulfill BossInfo page</h2>
+    return (
+      <div>
+        <NavBar>
+          fulfill BossInfo page
+        </NavBar>
+        <AvatarSelector></AvatarSelector>
+        <InputItem onChange={(v)=>this.onChange('title',v)}>
+          招聘职位
+        </InputItem>
+      </div>
+    )
   }
 }
 
